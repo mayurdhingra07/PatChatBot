bd0f14e78bc5b35f6e120ff282f916c4abcc9c2d
mayurdhingra07 <mayurdhingra1997@gmail.com>
2023-07-08 20:52:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from embedchain import OpenSourceApp
 # Create a new instance of the bot
 pat_chat_bot = OpenSourceApp()
 
-# Embed online resources
+# Add online resources
 # pat_chat_bot.add("youtube_video", "https://www.youtube.com/watch?v=<patent_related_video>")
-pat_chat_bot.add("https://www.uspto.gov/web/offices/pac/mpep/consolidated_laws.pdf")
+pat_chat_bot.add("pdf_file", "https://www.uspto.gov/web/offices/pac/mpep/consolidated_laws.pdf")
 # pat_chat_bot.add("web_page", "https://www.uspto.gov/patents")
 
-# Embed local resources
+# Add local resources
 pat_chat_bot.add_local("qna_pair", ("What is a utility patent?", "A utility patent is a patent that covers the creation of a new or improved—and useful—product, process, or machine. It generally permits its owner to exclude others from making, using, or selling the invention for a period of up to twenty years from the date of patent application filing, subject to the payment of maintenance fees."))
 
 # Create an input field in the Streamlit app for user input
